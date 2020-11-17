b0b75c67482c5a91dd9b212026fe318920c59f04
Olsi Hoxha <66084880+olsihoxha@users.noreply.github.com>
2020-11-17 19:09:19 +0100

    animation 1 added

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View ,StatusBar,Animated } from 'react-native';
+
+const Progress=({step,steps,height})=>{
+
+  const animatedValue=React.useRef(new Animated.Value(-1000)).current;
+  const reactive=React.useRef(new Animated.Value(-1000)).current;
+  const [width,setWidth]=useState(0);
+
+  React.useEffect(()=>{
+    Animated.timing(animatedValue,
+      {
+        toValue:reactive,
+        duration:300,
+        useNativeDriver:true,
+      }
+      ).start();
+  },[]);
+
+  React.useEffect(()=>{
+    reactive.setValue(-width + width * step/steps);
+  },[step,width])
+
+  return(
+    <View>
+    <Text style={{
+      fontSize:12,
+      fontWeight: 'bold',
+      marginBottom:4,
+    }}>
+    {step}/{steps}
+    </Text>
+    <View 
+    onLayout={e=>{
+      const newWidth=e.nativeEvent.layout.width;
+      setWidth(newWidth);
+    }}
+    style={{
+      height,
+      backgroundColor:"rgba(0,0,0,0.1)",
+      borderRadius:height,
+      overflow:"hidden",
+    }}>
+      <Animated.View
+        style={{
+          height,
+          width:'100%',
+          borderRadius:height,
+          backgroundColor:"pink",
+          position:'absolute',
+          top:0,
+          left:0,
+          transform:[{
+            translateX:animatedValue
+          }]
+        }}
+      />
+    </View>
+    </View>
+  )
+};
 
 export default function App() {
+  const [index,setIndex]=useState(0);
+
+  React.useEffect(()=>{
+    const interval=setInterval(()=>{
+      setIndex((index+1) % (10 + 1) );
+    },500);
+  
+    return ()=>{
+      clearInterval(interval);
+    };
+  },[index]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+     <Progress step={index} steps={10} height={20}/>
     </View>
   );
 }
@@ -15,7 +84,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
 });
